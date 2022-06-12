857f1b22f85bb6a64a63c6537a89820a7d558ec4
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2022-06-12 14:20:56 +0600

    setup next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+    exportPathMap: async function (defaultPathMap) {
+        // 🚩the only difference is here, we spread the default pathMap
+        const pathMap = { ...defaultPathMap };
+
+        for (const [path, config] of Object.entries(defaultPathMap)) {
+            if (path === '/') {
+                pathMap[path] = config;
+            } else {
+                pathMap[`${path}/index`] = config;
+            }
+        }
+
+        return pathMap;
+    },
+    images: {
+        loader: 'imgix',
+        path: '/',
+    },
+};
