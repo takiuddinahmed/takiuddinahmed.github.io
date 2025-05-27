4387b9061f8316c9ec0cd75436dd2cb25b538814
TakiUddinAhmed <takiuddinahmed@gmail.com>
2025-05-27 21:09:03 +0600

    Move mobile menu toggle script in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,33 @@ darkSwitch.addEventListener("change", function () {
   }
 });
 
+// Mobile Menu Toggle
+
+const mobileMenuBtn = document.getElementById("mobileMenuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+let isMenuOpen = false;
+
+mobileMenuBtn.addEventListener("click", () => {
+  isMenuOpen = !isMenuOpen;
+  if (isMenuOpen) {
+    mobileMenu.style.height = mobileMenu.scrollHeight + "px";
+    mobileMenuBtn.innerHTML = '<i class="fas fa-times text-xl"></i>';
+  } else {
+    mobileMenu.style.height = "0";
+    mobileMenuBtn.innerHTML = '<i class="fas fa-bars text-xl"></i>';
+  }
+});
+
+// Close mobile menu when clicking on a link
+const mobileMenuLinks = mobileMenu.querySelectorAll("a");
+mobileMenuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.style.height = "0";
+    mobileMenuBtn.innerHTML = '<i class="fas fa-bars text-xl"></i>';
+    isMenuOpen = false;
+  });
+});
+
 // Initialize GitHub calendar and activity feed
 document.addEventListener("DOMContentLoaded", function () {
   // GitHub Calendar
